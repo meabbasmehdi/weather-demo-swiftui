f110892adb50dd6bafc3490d5f5e5f3adeac871e
Abbas Mehdi <73042720+meabbasmehdi@users.noreply.github.com>
2024-11-26 16:59:46 +0500

    added initial project files

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  SwiftUI-Weather
+//
+//  Created by Abbas Mehdi on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
